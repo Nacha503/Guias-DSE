84ff9ceb5a986199498f18f2b10c643f80b44e54
Oscar Carranza <carranzav_oscar@hotmail.com>
2022-08-20 20:19:42 -0600

    se copio el proyecto MVCPelicula (tarea Lab01) a la carpeta Lab02

---
+++ Lab01/Ejercicio 2/Ejercicio 2/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace Ejercicio_2.Models
+{
+    public class Persona
+    {
+        public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime  FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+
+    }
+
+    public class PersonaDBContext: DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
