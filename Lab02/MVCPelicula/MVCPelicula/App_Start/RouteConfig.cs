a9602355416fcf7021e3a2b0d50f636e69988d64
Oscar Carranza <carranzav_oscar@hotmail.com>
2022-08-28 14:55:11 -0600

    se realizo el ejemplo introductorio de la guia 2

--- Lab02/MVCPelicula/MVCPelicula/App_Start/RouteConfig.cs
+++ Lab02/MVCPelicula/MVCPelicula/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace MVCPelicula
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            // este se agrego para trabajar en urls del tipo HelloWorld/Welcome?nombre=Juanito&apellido=2, tengo explicado esto en el txt del laboratorio 01
             routes.MapRoute(
                 name: "Hola",
                 url: "{controller}/{action}/{nombre}/{id}"
--- Lab02/MVCPelicula/MVCPelicula/Models/Pelicula.cs
+++ Lab02/MVCPelicula/MVCPelicula/Models/Pelicula.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
+using System.ComponentModel.DataAnnotations; // esto me servirá para darle formato a la fecha
+
 
 namespace MVCPelicula.Models
 {
@@ -11,6 +13,11 @@ namespace MVCPelicula.Models
     {
         public int ID { get; set; }
         public string Titulo { get; set; }
+
+
+        [Display(Name = "FechaLanzamiento de lanzamiento")] // . El atributo Display especifica qué mostrar para el nombre de un campo(en este caso, "Fecha de lanzamiento" en lugar de"FechaLanzamiento")
+        [DataType(DataType.Date)]  // El atributo DataType especifica el tipo de datos, en este caso es una fecha, por lo que no se muestra la información de tiempo almacenada en el campo
+        [DisplayFormat(DataFormatString = "{0 : yyyy-MM-dd}", ApplyFormatInEditMode = true)] // El atributo DisplayFormat es necesario para un error en el navegador Chrome que representa los formatos de fecha incorrectamente.
         public DateTime FechaLanzamiento { get; set; }
         public string Genero { get; set; }
         public decimal Precio { get; set; }
@@ -18,7 +25,7 @@ namespace MVCPelicula.Models
 
     public class PeliculaDBContext : DbContext
     {
-        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; } // como funciona el DbSet?
     }
 
 }
